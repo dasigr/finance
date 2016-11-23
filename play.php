ea7938389241431dec40d8878298356fcb1526db
Romualdo Dasig <dasig.rg@a5project.com>
2016-11-23 16:14:22 +0800

    Tested viewing saved expenses from database.

--- play.php
+++ play.php
@@ -16,11 +16,33 @@ $container = $kernel->getContainer();
 $container->enterScope('request');
 $container->set('request', $request);
 
+
+$em = $container->get('doctrine')->getManager();
+$repo = $em->getRepository('ExpenseBundle:Expense');
+
+$expense = $repo->findOneBy(array(
+    'category' => 'Food and Drinks'
+));
+
 $templating = $container->get('templating');
 
 echo $templating->render(
     'ExpenseBundle:Default:index.html.twig',
     array(
-        'title' => 'Expenses'
+        'title' => 'Expenses',
+        'expense' => $expense
     )
 );
+
+// use TrueApex\ExpenseBundle\Entity\Expense;
+
+// $expense = new Expense;
+// $expense->setCategory('Food and Drinks');
+// $expense->setDate(new \DateTime('today'));
+// $expense->setAmount(12000);
+// $expense->setNote('A new expense!');
+// $expense->setCreated(new \DateTime('today'));
+
+// $em = $container->get('doctrine')->getManager();
+// $em->persist($expense);
+// $em->flush();
--- src/TrueApex/ExpenseBundle/Controller/DefaultController.php
+++ src/TrueApex/ExpenseBundle/Controller/DefaultController.php
@@ -8,7 +8,20 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('ExpenseBundle:Expense');
+
+        $expense = $repo->findOneBy(array(
+            'category' => 'Food and Drinks'
+        ));
+
         $title = 'Expenses';
-        return $this->render('ExpenseBundle:Default:index.html.twig', array('title' => $title));
+        return $this->render(
+            'ExpenseBundle:Default:index.html.twig',
+            array(
+                'title' => $title,
+                'expense' => $expense
+            )
+        );
     }
 }
